90316fb7e2632005db486797746f63723f21e51c
jaaeeyoung <imitation_31@naver.com>
2022-04-05 18:07:56 +0900

    Improved

--- Python/15651_N과 M (3).py
+++ Python/15651_N과 M (3).py
@@ -8,59 +8,43 @@ Log
 ===================================================================================================================================
 ' 2022-04-04-MON : 문제 파악
                    문제 해결
+' 2022-04-05 TUE : 코드 최적화
 ===================================================================================================================================
 '''
 '''
 Algorithm
+itertools library 이용
+1. product(중복 순열) 이용해 모든 조합 만들기
+2. 리스트 정렬 후 출력
+
 백트래킹 사용
-1. 1부터 N까지의 숫자를 리스트에 하나씩 넣음
-2. 리스트의 길이가 M이 되면 answer에 추가하고 리스트의 마지막 원소 확인
-2.1. 리스트 마지막 원소가 N이 아니면 pop()한 번 하고 다음 index의 값 넣기
-2.2. 리스트 마지막 원소가 N이면 끝에서부터 연속으로 N이 들어있는 개수만큼 pop하고 마지막 원소를 다음 index로 설정
-3. answer 출력
+1. for문을 돌면서 stack에 숫자 추가하고 재귀함수 호출
+2. 재귀함수 return되면 pop
+3. stack의 길이가 M이 되면 stack print
 '''
+from itertools import product
 
-N, M = map(int, input().split())
-
-temp = []
-answer = []
-num = 1
-while num < N+1:
-    # temp에 num 추가
-    temp.append(num)
+def back_tracking():
     
-    # temp의 길이가 M이 되면
-    if len(temp) == M:
-        # answer에 추가
-        answer.append(temp[:])
-        
-        # temp의 마지막 원소 확인
-        if temp[-1] == N: # 마지막 원소가 마지막 숫자이면
-            # 마지막값이 N이 아닐 때 까지 pop
-            while temp and temp[-1] == N:
-                # temp가 빈리스트이면 break
-                if not temp:
-                    break
-                temp.pop()
+    # stack 길이가 M이 되면 print
+    if len(stack) == M:
+        print(' '.join(list(map(str, stack))))
+    else:
+        # 1부터 N까지 순서대로 stack에 append
+        for num in range(1, N+1):
+            stack.append(num)
+            # 재귀함수 호출
+            back_tracking()
+            # 재귀함수 return되면 pop
+            stack.pop()
 
-            # temp가 빈리스트이면 break
-            if not temp:
-                break
-            # num은 마지막 원소 +1로 설정
-            num = temp[-1]+1
-            # 한 번 더 pop한 후
-            temp.pop()
-            # num의 값 append
-            temp.append(num)
-            # num은 1로 초기화
-            num = 1
-        else: # 마지막 원소가 마지막 숫자가 아니었다면
-            # num을 마지막 원소 + 1로 설정
-            num = temp[-1]+1
-            # 한 번만 pop
-            temp.pop()
-
-for nums in answer:
+def use_product():
+    nums = list(product(range(1, N+1), repeat = M))
+    nums.sort()
     for num in nums:
-        print(num, end = ' ')
-    print()
+        print(' '.join(list(map(str, num))))
+        
+N, M = map(int, input().split())
+stack = []
+# back_tracking()
+use_product()
