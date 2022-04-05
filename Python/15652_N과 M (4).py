f9958252616882fe195fe332100186e7bed28d77
jaaeeyoung <imitation_31@naver.com>
2022-04-05 18:23:36 +0900

    Improved

--- Python/15652_N과 M (4).py
+++ Python/15652_N과 M (4).py
@@ -8,56 +8,37 @@ Log
 ===================================================================================================================================
 ' 2022-04-04-MON : 문제 파악
                    문제 해결
+' 2022-04-05 TUE : 코드 최적화
 ===================================================================================================================================
 '''
 '''
 Algorithm
 백트래킹 사용
-1. 1부터 N까지의 숫자를 리스트에 하나씩 넣음
-    -> 현재 숫자가 리스트의 마지막 원소보다 작으면 무시
-2. 리스트의 길이가 M이 되면 answer에 추가하고 리스트의 마지막 원소 확인
-2.1. 리스트 마지막 원소가 N이 아니면 pop()한 번 하고 다음 index의 값 넣기
-2.2. 리스트 마지막 원소가 N이면 끝에서부터 연속으로 N이 들어있는 개수만큼 pop하고 마지막 원소를 다음 index로 설정
-3. answer 출력
+1. for문을 돌면서 숫자 추가
+1.1. stack이 비어있으면 바로 추가
+1.2. stack이 비어있지 않으면 num이 stack의 마지막 원소보다 크거나 같을 때만 추가
+2. 재귀함수 호출
+3. 재귀함수 return되면 pop
+4. stack의 길이가 M이 되면 print
 '''
 
+def back_tracking():
+    
+    # stack의 길이가 M이 되면 print
+    if len(stack) == M:
+        print(' '.join(list(map(str, stack))))
+    else:
+        for num in range(1, N+1):
+            # stack이 비어있으면 그냥 추가
+            if not stack:
+                stack.append(num)
+                back_tracking()
+                stack.pop()
+            elif stack[-1] <= num: # 그렇지 않으면 stack의 마지막 원소보다 크거나 같을 때만 추가
+                stack.append(num)
+                back_tracking()
+                stack.pop()
+
 N, M = map(int, input().split())
-
-temp = []
-answer = []
-num = 1
-while num < N+1:
-    # 해당 숫자가 temp의 마지막 원소보다 작으면 continue
-    if temp and num < temp[-1]:
-        num += 1
-        continue
-    
-    # temp에 num 추가
-    temp.append(num)
-    
-    # temp의 길이가 M이 되면
-    if len(temp) == M:
-        # answer에 temp 저장
-        answer.append(temp[:])
-        
-        # 마지막 원소 확인
-        if temp[-1] == N: # 마지막 원소가 N이면
-            # 마지막 원소가 N이 아닐 때까지 pop
-            while temp and temp[-1] == N:
-                temp.pop()
-            if temp:
-                num = temp[-1]+1
-                temp.pop()
-                temp.append(num)
-            else:
-                break
-            num = 1
-        else: # 마지막 원소가 N이 아니면
-            # num값 갱신하고 한번만 pop
-            num = temp[-1] + 1
-            temp.pop()
-
-for nums in answer:
-    for num in nums:
-        print(num, end = ' ')
-    print()
+stack = []
+back_tracking()
